6470bbe28c0588173db9802ff2a4f29f24800458
warnermo <morgan.warner@fxinnovation.com>
2018-03-28 10:29:31 -0400

    adding inspec test and proper node attributes in kitchen

--- recipes/kitchen.rb
+++ recipes/kitchen.rb
@@ -7,8 +7,8 @@
 #
 
 windows_fx_proxy 'default' do
-  ip       node['windows_fx']['ip']
-  port     node['windows_fx']['port']
-  override node['windows_fx']['override']
+  ip       node['windows_fx']['proxy']['ip']
+  port     node['windows_fx']['proxy']['port']
+  override node['windows_fx']['proxy']['override']
   action   :create
 end
---
+++ test/integration/windows/install.rb
@@ -0,0 +1,16 @@
+#
+# Inspec test for windows_fx on rhel platform family
+#
+# the Inspec refetence, with examples and extensive documentation, can be
+# found at https://inspec.io/docker/reference/resources/
+#
+control 'windows_fx - windows - 01' do
+  title 'The computer should have the proxy set'
+  describe powershell('$(get-itemproperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable).ProxyEnable') do
+    its('stdout') { should match(/0/) }
+  end
+  describe powershell('$(get-itemproperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer).ProxyServer') do
+    # Not matching with regex to allow for IPv6 addresses
+    its('stdout') { should exist }
+  end
+end
