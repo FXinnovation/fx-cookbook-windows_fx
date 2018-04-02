19754f34850ecba0e718c9337ee03d8b70ef3731
warnermo <morgan.warner@fxinnovation.com>
2018-04-02 12:08:51 -0400

    correct inspec

--- test/integration/windows/install.rb
+++ test/integration/windows/install.rb
@@ -5,12 +5,11 @@
 # found at https://inspec.io/docker/reference/resources/
 #
 control "windows_fx - #{os.name} #{os.release} - 01" do
-  title 'The computer should have the proxy set'
-  describe powershell('$(get-itemproperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable).ProxyEnable') do
-    its('stdout') { should match(/0/) }
+  describe powershell('$(get-itemproperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyEnable).ProxyEnable') do
+    its('stdout') { should match(/1/) }
   end
-  describe powershell('$(get-itemproperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer).ProxyServer') do
+  describe powershell('$(get-itemproperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxyServer).ProxyServer') do
     # Not matching with regex to allow for IPv6 addresses
-    its('stdout') { should exist }
+    its('stdout') { should match(/^\d{1,3}(\.\d{1,3}){3}:\d+/) }
   end
 end
