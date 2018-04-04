f3d2ee2111e60b3cdac13860360ff67a3111e013
warnermo <morgan.warner@fxinnovation.com>
2018-04-04 12:40:26 -0400

    updating cookbook verison in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'FXinnovation'
 maintainer_email 'cloudsquad@fxinnovation.com'
 description      'Provides configuration options for a windows machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 chef_version     '>= 12.14' if respond_to?(:chef_version)
 issues_url       'https://bitbucket.org/fxadmin/public-common-cookbook-windows_fx/issues'
 source_url       'https://bitbucket.org/fxadmin/public-common-cookbook-windows_fx'
